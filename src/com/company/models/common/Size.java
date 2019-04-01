72fb83d79f4c3976b4aeffd941abdddcca774136
Stoyan Terziev <savergad@abv.bg>
2019-04-01 18:38:28 +0300

    implemented StoryImpl without Interfaces

---
+++ src/com/company/models/common/Size.java
@@ -0,0 +1,7 @@
+package com.company.models.common;
+
+public enum Size {
+    Large,
+    Medium,
+    Small
+}
--- src/com/company/models/workItem/StoryImpl.java
+++ src/com/company/models/workItem/StoryImpl.java
@@ -1,6 +1,54 @@
 package com.company.models.workItem;
 
+import com.company.models.common.Priority;
+import com.company.models.common.Size;
+import com.company.models.common.Status;
+import com.company.models.contracts.unit.Member;
 import com.company.models.contracts.workItem.Story;
 
 public class StoryImpl extends WorkItemBase implements Story {
+
+    private Priority priority;
+    private Size size;
+    private Member assignee;
+
+    public StoryImpl(int id, String title, String description, Status status, Priority priority, Size size, Member assignee) {
+        super(id, title, description, status);
+       setAssignee(assignee);
+       setPriority(priority);
+       setSize(size);
+       setStatus(status);
+    }
+
+    public Priority getPriority() {
+        return priority;
+    }
+
+    private void setPriority(Priority priority) {
+        this.priority = priority;
+    }
+
+    public Size getSize() {
+        return size;
+    }
+
+    private void setSize(Size size) {
+        this.size = size;
+    }
+
+    public Member getAssignee() {
+        return assignee;
+    }
+
+    private void setAssignee(Member assignee) {
+        this.assignee = assignee;
+    }
+
+
+    @Override
+    void setStatus(Status status) {
+        if(status != Status.NotDone && status != Status.InProgress && status != Status.Done) {
+            throw new IllegalArgumentException();
+        }
+    }
 }
