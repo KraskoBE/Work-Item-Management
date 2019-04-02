62b7fb5f24948c34ac395fa2ca4210f2c8fa5e1c
Stoyan Terziev <savergad@abv.bg>
2019-04-02 14:56:47 +0300

    implemented StoryImpl without Interfaces

--- src/com/company/models/contracts/workItem/Story.java
+++ src/com/company/models/contracts/workItem/Story.java
@@ -1,4 +1,15 @@
 package com.company.models.contracts.workItem;
 
+import com.company.models.common.Priority;
+import com.company.models.common.Size;
+import com.company.models.contracts.unit.Member;
+
 public interface Story extends WorkItem{
+
+    public Priority getPriority();
+
+    public Size getSize();
+
+    public Member getAssignee();
+
 }
