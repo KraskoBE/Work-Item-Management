3336abc013e97c367697dd48d028279e8956c46d
Stoyan Terziev <savergad@abv.bg>
2019-04-02 15:16:06 +0300

    implemented BoardImpl

--- src/com/company/models/unit/BoardImpl.java
+++ src/com/company/models/unit/BoardImpl.java
@@ -4,4 +4,20 @@ package com.company.models.unit;
 import com.company.models.contracts.unit.Board;
 
 public class BoardImpl extends UnitBase implements Board {
+
+    private static final int NAME_MIN_LENGTH = 5;
+    private static final int NAME_MAX_LENGTH = 10;
+
+    public BoardImpl(String name) {
+        super(name);
+    }
+
+    @Override
+    void setName(String name) {
+        if(name.length() < NAME_MIN_LENGTH ||  name.length() > NAME_MAX_LENGTH) {
+            throw new IllegalArgumentException();
+        }
+        this.name = name;
+    }
+
 }
