97551d6c117a5c0042727dbd7a60b7616dd9ea6a
Krasen <krasenvachev3@gmail.com>
2019-04-16 22:00:12 +0300

    Added test for CreateFeedback Command

--- src/Tests/CreateFeedbackCmd_Should.java
+++ src/Tests/CreateFeedbackCmd_Should.java
@@ -4,6 +4,7 @@ import com.company.engine.EngineConstants;
 import com.company.engine.EngineImpl;
 import com.company.engine.FactoryImpl;
 import com.company.engine.commands.CreateBugCmd;
+import com.company.engine.commands.CreateFeedbackCmd;
 import com.company.models.TeamImpl;
 import com.company.models.contracts.Team;
 import com.company.models.contracts.unit.Board;
@@ -16,7 +17,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class CreateBugCmd_Should {
+public class CreateFeedbackCmd_Should {
     private EngineImpl engine;
     private FactoryImpl factory;
     private List<String> parameters;
@@ -37,20 +38,20 @@ public class CreateBugCmd_Should {
     }
 
     @Test
-    public void createBugWithValidParameters() {
-        parameters = Arrays.asList("ImeNaBugaa", "description na bug", "high", "critical", "board", "otbor");
+    public void createFeedbackWithValidParameters() {
+        parameters = Arrays.asList("ImeNaFeedback", "description na bug", String.valueOf(100), "board", "otbor");
 
-        String result = CreateBugCmd.execute(engine, factory, parameters);
-        String expected = String.format(EngineConstants.BugCreatedSuccessMessage, "ImeNaBugaa", 0);
+        String result = CreateFeedbackCmd.execute(engine, factory, parameters);
+        String expected = String.format(EngineConstants.FeedBackSuccessMessage, "ImeNaFeedback", 0);
 
         Assert.assertEquals(expected, result);
     }
 
     @Test
     public void errorWhenTeamDoesNotExist() {
-        parameters = Arrays.asList("ImeNaBugaa", "description na bug", "high", "critical", "board", "nemaOtbor");
+        parameters = Arrays.asList("ImeNaFeedback", "description na bug", String.valueOf(100), "board", "nemaOtbor");
 
-        String result = CreateBugCmd.execute(engine, factory, parameters);
+        String result = CreateFeedbackCmd.execute(engine, factory, parameters);
         String expected = String.format(EngineConstants.TeamDoesNotExistErrorMessage, "nemaOtbor");
 
         Assert.assertEquals(expected, result);
@@ -58,9 +59,9 @@ public class CreateBugCmd_Should {
 
     @Test
     public void errorWhenBoardIsNotOnTheTeam() {
-        parameters = Arrays.asList("ImeNaBugaa", "description na bug", "high", "critical", "nemaBoard", "otbor");
+        parameters = Arrays.asList("ImeNaFeedback", "description na bug", String.valueOf(100), "nemaBoard", "otbor");
 
-        String result = CreateBugCmd.execute(engine, factory, parameters);
+        String result = CreateFeedbackCmd.execute(engine, factory, parameters);
         String expected = String.format(EngineConstants.BoardIsNotOnTheTeamErrorMessage, "nemaBoard", team.getName());
 
         Assert.assertEquals(expected, result);
@@ -68,9 +69,9 @@ public class CreateBugCmd_Should {
 
     @Test
     public void errorWhenMoreParametersArePassed() {
-        parameters = Arrays.asList("a", "b", "c", "d", "e", "f", "g");
+        parameters = Arrays.asList("a", "b", "c", "d");
 
-        String result = CreateBugCmd.execute(engine, factory, parameters);
+        String result = CreateFeedbackCmd.execute(engine, factory, parameters);
         String expected = EngineConstants.InvalidNumberOfParameters;
 
         Assert.assertEquals(expected, result);
@@ -78,9 +79,9 @@ public class CreateBugCmd_Should {
 
     @Test
     public void errorWhenLessParametersArePassed() {
-        parameters = Arrays.asList("a", "b", "c", "d", "e");
+        parameters = Arrays.asList("a", "b", "c", "d", "e", "f", "g");
 
-        String result = CreateBugCmd.execute(engine, factory, parameters);
+        String result = CreateFeedbackCmd.execute(engine, factory, parameters);
         String expected = EngineConstants.InvalidNumberOfParameters;
 
         Assert.assertEquals(expected, result);
