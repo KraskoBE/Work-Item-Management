709cdd291a56c09dba40301e287c82f930fb081c
Krasen <krasenvachev3@gmail.com>
2019-04-16 22:54:34 +0300

    Fixed Show All People command test

--- src/Tests/ShowAllPeopleCmd_Should.java
+++ src/Tests/ShowAllPeopleCmd_Should.java
@@ -3,6 +3,7 @@ package Tests;
 import com.company.engine.EngineConstants;
 import com.company.engine.EngineImpl;
 import com.company.engine.commands.ShowAllPeopleCmd;
+import com.company.models.contracts.unit.Member;
 import com.company.models.unit.MemberImpl;
 import org.junit.Assert;
 import org.junit.Before;
@@ -12,45 +13,30 @@ import java.util.ArrayList;
 
 public class ShowAllPeopleCmd_Should {
     private EngineImpl engine;
-    private MemberImpl member;
 
     @Before
     public void beforeFunc() {
         engine = new EngineImpl();
-        member = new MemberImpl("krasen");
     }
 
     @Test
     public void ShowAllPeople() {
-        engine.getMembers().put(member.getName(), member);
+        Member member1 = new MemberImpl("krasen");
+        Member member2 = new MemberImpl("stoyan");
 
+        engine.getMembers().put(member1.getName(), member1);
+        engine.getMembers().put(member2.getName(), member2);
 
         String result = ShowAllPeopleCmd.execute(engine);
-
-        StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append("[PEOPLE]\n");
-        engine.getMembers().forEach((k, v) -> stringBuilder.append(v.getName()).append('\n'));
-
-        String expected = stringBuilder.toString().trim();
-
+        String expected = "[PEOPLE]\n" + String.join("\n", member1.getName(), member2.getName());
         Assert.assertEquals(expected, result);
-
     }
 
     @Test
     public void errorNoMembers() {
-
         String result = ShowAllPeopleCmd.execute(engine);
-
-        StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append("[PEOPLE]\n");
-        engine.getMembers().forEach((k, v) -> stringBuilder.append(v.getName()).append('\n'));
-
         String expected = EngineConstants.NoMembersErrorMessage;
 
         Assert.assertEquals(expected, result);
-
     }
-
-
 }
